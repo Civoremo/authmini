be15c27ca34fc6e96c46b865f396a56fbab8c2e1
Civoremo <omerovic_n@hotmail.com>
2019-01-19 11:55:17 -0500

    register/login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
@@ -8,14 +9,47 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+server.post('/api/register', (req, res) => {
+  const creds = req.body;
+
+  const hashedPassword = bcrypt.hashSync(creds.password, 14);
+
+  creds.password = hashedPassword;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
 });
+
+server.post('/api/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username }).first()
+    .then(user => {
+      if(user && bcrypt.compareSync(creds.password, user.password)) {
+        // password match and user exists by that username
+        res.status(200).json({message: 'welcome!'});
+      } else {
+        // either username is invalid or password is wrong
+        res.status(401).json({message: 'you shall not pass!!'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });  
+});
+
 
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
